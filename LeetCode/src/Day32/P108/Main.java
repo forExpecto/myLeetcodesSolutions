a5c579da19338763574383f94923575fb3867267
David <1838844858@qq.com>
2024-09-13 21:39:21 +0800

    p108

--- LeetCode/src/Day32/P108/Main.java
+++ LeetCode/src/Day32/P108/Main.java
@@ -1,10 +1,9 @@
 package Day32.P108;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        System.out.println(n);
+        int []nums = new int[]{5,1,2,3,4};
+        Solution solution = new Solution();
+        solution.sortedArrayToBST(nums);
     }
 }
--- LeetCode/src/Day32/P108/Solution.java
+++ LeetCode/src/Day32/P108/Solution.java
@@ -1,4 +1,18 @@
 package Day32.P108;
 
-public class Solution {
+class Solution {
+    public TreeNode createTree(int []nums,int left,int right){
+        if(left>right) return null;
+        if(left==right) return new TreeNode(nums[left]);
+        int index = (left+right)/2;
+        TreeNode root = new TreeNode(nums[index]);
+        root.left = createTree(nums,left,index-1);
+        root.right = createTree(nums,index+1,right);
+        return root;
+    }
+
+    public TreeNode sortedArrayToBST(int[] nums) {
+        TreeNode root = createTree(nums,0,nums.length-1);
+        return root;
+    }
 }
